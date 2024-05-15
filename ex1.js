295623d70e88063da7dd63b8680edd8802d615cb
04_Apiwat (Tee/ตี๋) <apiwat.pienwisetkaew@gmail.com>
2024-05-15 13:32:21 +0700

    first-attemp

---
+++ ex1.js
@@ -0,0 +1,27 @@
+//Exercise1//
+
+let myAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+
+const myAlphabetLength = () => console.log(myAlphabet.length);
+
+myAlphabetLength();
+
+//Exercise1.2//
+
+const planets = ['mars','earth','saturn','venus','neptune'];
+
+console.log(planets);
+
+planets.forEach(planet => console.log(planet));
+
+console.log(planets[0]);
+console.log(planets[1]);
+console.log(planets[2]);
+console.log(planets[3]);
+console.log(planets[4]);
+
+//Exercise1.3//
+
+let myArr = [ 1, 2, 'One', true];
+
+myArr.forEach(item => console.log(item));
